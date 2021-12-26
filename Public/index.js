216cc6dea12ad0856811bae90f595054ddecd926
sarangKaliyath <86410007+sarangKaliyath@users.noreply.github.com>
2021-12-26 12:41:06 +0530

    css changes

--- Public/index.js
+++ Public/index.js
@@ -1,6 +1,6 @@
 const socket = io();
 
-// const username = prompt("Enter username");
+const username = prompt("Enter username");
 const input = document.querySelector(".input");
 const button = document.querySelector(".button");
 const display = document.querySelector(".display_box");
@@ -13,26 +13,30 @@ button.addEventListener("click", () => {
   }
 });
 
+let flag = true;
+
 socket.on("input_message", function (data) {
+  flag = !flag;
+
   const li = document.createElement("li");
   li.textContent = `${data.name}: ${data.data}`;
+  flag
+    ? li.setAttribute("class", "text_first")
+    : li.setAttribute("class", "text_second");
   ul.appendChild(li);
 });
 
-socket.on("user_joined", function (data) {
+socket.on("user_joined", function ({ name }) {
   const li = document.createElement("li");
-  const h1 = document.createElement("h1");
-  h1.textContent = `${data} has joined chat`;
-  li.appendChild(h1);
+  li.textContent = `${name} has joined chat`;
+  li.setAttribute("class", "user_joined");
   ul.appendChild(li);
 });
 
-socket.on("user_left", function (data) {
+socket.on("user_left", function ({ name }) {
   const li = document.createElement("li");
-  const h1 = document.createElement("h1");
-
-  h1.textContent = `${data} has left chat`;
-  li.appendChild(h1);
+  li.textContent = `${name} has left chat`;
+  li.setAttribute("class", "user_left");
   ul.appendChild(li);
 });
 
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const app = express();
-// const app = require("express")();
-
-// const { rmSync } = require("fs");
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -11,23 +8,20 @@ const port = 3000;
 
 const user_data = {};
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "./index.html");
-// });
 const path = require("path");
 app.use(express.static(path.join(__dirname, "Public")));
 
 io.on("connection", (socket) => {
   socket.on("user_joined", function (data) {
     user_data[socket.id] = data;
-    socket.broadcast.emit("user_joined", data);
+    socket.broadcast.emit("user_joined", { name: user_data[socket.id] });
   });
 
   socket.on("input_message", (data) => {
     io.emit("input_message", { data: data, name: user_data[socket.id] });
   });
   socket.on("disconnect", (data) => {
-    socket.broadcast.emit("user_left", user_data[socket.id]);
+    socket.broadcast.emit("user_left", { name: user_data[socket.id] });
   });
 });
 
